a7f40b15d015a927f927438ee2c84c0044fc8408
Abdulwahab Isam <abdoi94.iq@gmail.com>
2021-11-27 11:35:18 +0300

    avd-run: run manually avd from printed list
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- avd-run.py
+++ avd-run.py
@@ -1,9 +1,8 @@
 from subprocess import PIPE, Popen
 
-process = Popen(
-            "emulator -list-avds",
-            shell=True,
-            stdout=PIPE,
-        )
+process = Popen( "emulator -list-avds", shell=True, stdout=PIPE,)
 stdout, stderr = process.communicate()
 print(stdout.decode())
+
+avd = input("Enter AVD name to run: ")
+avdrun =  Popen( f"emulator -avd {avd}", shell=True, stdout=PIPE,)
